6d660729bf16a3b2ef4a6f6272455be510967904
Sidney C Moraes <sidneycamargo@gmail.com>
2020-04-10 02:01:31 -0300

    gostack first commit

---
+++ conceitos-dev/backend/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end 
+ * POST: Cria uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+app.get('/projects', (request, response) => {
+  return response.json( [
+    'Projeto 1',
+    'Projeto 2'
+  ] );
+});
+
+app.post('/projects', (request, response) => {
+  return response.json( [
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ] );
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json( [
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ] );
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json( [
+    'Projeto 2',
+    'Projeto 3'
+  ] );
+});
+
+app.listen(3333, () => {
+  console.log('🔬️ Back-end started');
+});
