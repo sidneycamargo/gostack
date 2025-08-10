b6126ea8696c9418b52a7a5a9aadd76f224a8d99
Sidney C Moraes <sidneycamargo@gmail.com>
2025-08-10 10:14:33 +0200

    Modified handleRemoveRepository

--- conceitos-dev/frontend/src/App.js
+++ conceitos-dev/frontend/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect} from 'react';
-import api from './services/api';
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
-import './App.css';
+import "./App.css";
 
-import Header from './components/Header';
+import Header from "./components/Header";
 
 /**
  * Conceitos importantes do React:
@@ -18,7 +18,7 @@ function App() {
   console.log(projects);
 
   useEffect(() => {
-    api.get('projects').then(response => {
+    api.get("projects").then((response) => {
       setProjects(response.data);
     });
   }, []);
@@ -26,27 +26,26 @@ function App() {
   async function handleAddProject() {
     //projects.push(`Novo Projeto ${Date.now()}`);
     // setProjects([ ... projects, `Novo Projeto ${Date.now()}`])
-    const response = await api.post('projects', {
+    const response = await api.post("projects", {
       title: `Novo Projeto ${Date.now()}`,
-      owner: "Sidney Camargo Moraes"6
+      owner: "Sidney Camargo Moraes",
     });
 
     const project = response.data;
 
-    setProjects([ ... projects, project]);
+    setProjects([...projects, project]);
 
     console.log(projects);
   }
 
   async function handleRemoveRepository(id) {
-
     const projectTmp = projects;
 
-    const projectIndex = projects.findIndex( project => project.id === id);
+    const projectIndex = projects.findIndex((project) => project.id === id);
 
-    projectTmp.splice( projectIndex, 1);
+    projectTmp.splice(projectIndex, 1);
 
-    setProjects( projectTmp);
+    setProjects(projectTmp);
   }
 
   return (
@@ -54,13 +53,15 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map(project => <li key={project.id}>{project.title}</li> )}
-        <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+        {projects.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+        <button onClick={() => handleRemoveRepository(1)}>Remover</button>
       </ul>
 
-      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
